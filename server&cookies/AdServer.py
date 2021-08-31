8a6bd92066abde2eee933eb202f174fce0a0ab15
KeaiPandas <zkphao3@gmail.com>
2021-08-31 09:41:29 -0400

    手写cookie

--- server&cookies/AdServer.py
+++ server&cookies/AdServer.py
@@ -1,7 +1,6 @@
 from http.server import HTTPServer,BaseHTTPRequestHandler
 from http import cookies
 from urllib.parse import parse_qs
-from html import escape as html_escape
 
 form = '''
 <!DOCTYPE html>
@@ -15,11 +14,12 @@ form = '''
 <body>
     <p>
         {}
-    </p>
+    <p>
     <form method="POST">
         <label>为了更好的为您服务，请输入年龄
             <input type="text" name="yourage">
         </label>
+        <br>
         <button type="submit">傻乎乎的提交年龄~</button>
     </form>
 </body>
@@ -27,7 +27,7 @@ form = '''
 '''
 
 class AgeHandler(BaseHTTPRequestHandler):
-    def do_POST(self) -> None:
+    def do_POST(self):
         length = int(self.headers.get('Content-length',0))
         data = self.rfile.read(length).decode()
         your_age = parse_qs(data)['yourage'][0]
@@ -39,4 +39,35 @@ class AgeHandler(BaseHTTPRequestHandler):
 
         self.send_response(303)
         self.send_header('Location','/')
-        self.send_header('Set-Cookie',c['yourage'])
+        self.send_header('Set-Cookie',c['yourage'].OutputString())
+        self.end_headers()
+    
+    def do_GET(self):
+        message = "您好，请您输入您的年龄～"
+
+        if "cookie" in self.headers:
+            try:
+                c=cookies.SimpleCookie(self.headers['cookie'])
+                age = c['yourage'].value
+                if int(age) > 40:
+                    message = "快服用XXX，延缓衰老,您值得拥有~"
+                elif int(age) >25:
+                    message = "XXXX护肤品"
+                else:
+                    message = "XXXX网校"
+
+            except (KeyError,cookies.CookieError) as e:
+                message = "不好意思，我没有找到您的信息。"
+                print(e)
+        
+        self.send_response(200)
+        self.send_header('Content-type','text/html; charset=utf-8')
+        self.end_headers()
+
+        mesg = form.format(message)
+        self.wfile.write(mesg.encode())
+
+if __name__ == '__main__':
+    server_address = ('',9999)
+    httpd = HTTPServer(server_address,AgeHandler)
+    httpd.serve_forever()
