e0d01085b54f442193a849018751e0d71122f14e
KeaiPandas <zkphao3@gmail.com>
2021-10-03 23:39:13 +0800

    vue学习

--- vue_cli/vue_test/src/App.vue
+++ vue_cli/vue_test/src/App.vue
@@ -1,22 +1,16 @@
 <template>
-    <div>
-        <img src="./assets/logo.png" alt="logo">
-        <School></School>
-        <Student></Student>
-    </div>
+  <div>
+    <School/>
+  </div>
 </template>
 
 <script>
-    //引入组件
-    import School from './components/School'
-    import  Student from './components/Student'
-    export default {
-        name:'App',
-        components:{
-            School,
-            Student
-        }
-    }
+//引入School组件
+import School from './components/School'
+export default {
+  name: 'App',
+  components: {School}
+}
 </script>
 
 <style>
--- vue_cli/vue_test/src/main.js
+++ vue_cli/vue_test/src/main.js
@@ -1,16 +1,12 @@
-/*
-  该文件是整个项目的入口文件
-*/
 //引入Vue
 import Vue from 'vue'
-//引入App组件，它是所有组件的父组件
+//引入App
 import App from './App.vue'
-//关闭vue的生产提示
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
-//创建vue实例对象---vm
+//创建VM
 new Vue({
-  //将app组件放入容器中
-  el:'app',
-  render: h => h(App),
+  el:'#app',
+  render: h => h(App)
 })
