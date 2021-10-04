3f5a0f7286d823cb9f13fe1529ca108a5b745c8b
KeaiPandas <zkphao3@gmail.com>
2021-10-04 16:14:11 +0800

    学习plugins

--- vue_cli/vue_test/src/components/School.vue
+++ vue_cli/vue_test/src/components/School.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="student">
-    <h2 @click="showName">学校名称： {{ name }}</h2>
+    <h2>学校名称： {{ name}}</h2>
     <h2>学校地址： {{ address }}</h2>
   </div>
 </template>
 
 <script>
-import {mixin} from '../mixin'
 export default {
   name:'Student',
   data() {
@@ -15,6 +14,5 @@ export default {
       address: '深圳'
     }
   },
-  mixins:[mixin]
 }
 </script>
--- vue_cli/vue_test/src/components/Student.vue
+++ vue_cli/vue_test/src/components/Student.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="student">
-    <h2 @click="showName">学生名称： {{ name }}</h2>
+    <h2>学生名称： {{ name }}</h2>
     <h2>学生性别： {{ sex }}</h2>
   </div>
 </template>
 
 <script>
 //引入一个混合
-import {mixin} from '../mixin'
 export default {
   name:'Student',
   data() {
@@ -15,7 +14,6 @@ export default {
       name: '帅比',
       sex:'男'
     }
-  },
-  mixins: [mixin]
+  }
 }
 </script>
