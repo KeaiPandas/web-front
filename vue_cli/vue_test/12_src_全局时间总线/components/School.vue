ef0869d757b732bb0071658e9c0da650fbb2b932
KeaiPandas <zkphao3@gmail.com>
2021-10-09 21:54:34 +0800

    消息订阅与发布

---
+++ vue_cli/vue_test/12_src_全局时间总线/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="school">
+		<h2>学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+		<!-- <button @click="sendStudentName">把学生名给School组件</button> -->
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'School',
+		props:['getSchoolName'],
+		data() {
+			return {
+				name:'尚硅谷',
+				address:'北京',
+			}
+		},
+		mounted() {
+			this.$bus.$on('hello',(data)=>{
+				console.log('我是school组件收到了数据',data)
+			})
+		},
+		beforeDestroy() {
+			this.$bus.$off('hello')
+		}
+	}
+</script>
+
+<style scoped>
+	.school{
+		background-color: skyblue;
+		padding: 5px;
+	}
+</style>
