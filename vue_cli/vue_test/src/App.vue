f71287cf12806c1a53e9aa76fac03b34a2f356ff
KeaiPandas <zkphao3@gmail.com>
2021-10-21 22:02:20 +0800

    配置代理

--- vue_cli/vue_test/src/App.vue
+++ vue_cli/vue_test/src/App.vue
@@ -1,15 +1,26 @@
 <template>
-	<div >
-		<Test />
+	<div>
+		<button @click="getStudents"></button>
 	</div>
 </template>
 
 <script>
-	import Test from './components/test'
+	import axios from 'axios'
 
 	export default {
 		name:'App',
-		components:{Test},
+		method: {
+			getStudents(){
+				axios.get('http://localhost:5000/student').then(
+					response => {
+						console.log('请求成功了',response.data)
+					},
+					error => {
+						console.log('请求失败了',error.message)
+					}
+				)
+			}
+		}
 	}
 </script>
 
