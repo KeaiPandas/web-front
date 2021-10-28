72361bd4e7d558ca58c0eb6d27d5a37fd2faa191
KeaiPandas <zkphao3@gmail.com>
2021-10-28 21:42:15 +0800

    router学习

--- vue_cli/vue_test/src/main.js
+++ vue_cli/vue_test/src/main.js
@@ -2,15 +2,18 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
+
+import VueRouter from 'vue-router'
+
+import router from './router'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
 
 //创建VM
 new Vue({
   el:'#app',
   render: h => h(App),
-  beforeCreate() {
-    Vue.prototype.$bus = this //安装全局事件总线
-  }
+  router: router
 })
